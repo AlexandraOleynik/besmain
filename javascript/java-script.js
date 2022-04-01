7a07d6f8bb23d9f69d1c2eb8e3ff62117a102320
AlexandraOleynik <99476697+AlexandraOleynik@users.noreply.github.com>
2022-04-01 12:36:15 +0300

    Add files via upload

---
+++ javascript/java-script.js
@@ -0,0 +1,79 @@
+
+$(document).ready(function(){
+$("#square1").click(function(){
+    $("#start1").css("display", "none");
+    $("#screen2").css("display", "block");
+});
+});
+$(document).ready(function(){
+$("#square22").click(function(){
+    $("#screen2").css("display", "none");
+    $("#screenroku").css("display", "block");
+});
+});
+$(document).ready(function(){
+$("#square33").click(function(){
+    $("#screen2").css("display", "none");
+    $("#screenoni").css("display", "block");
+});
+});
+$(document).ready(function(){
+$("#square44").click(function(){
+    $("#screen2").css("display", "none");
+    $("#screenkitsu").css("display", "block");
+});
+});
+$(document).ready(function(){
+$("#square55").click(function(){
+    $("#screen2").css("display", "none");
+    $("#screenshishi").css("display", "block");
+});
+});
+$(document).ready(function(){
+$("#buttonroku1").click(function(){
+    $("#screenroku").css("display", "none");
+    $("#start1").css("display", "block");
+});
+});
+$(document).ready(function(){
+$("#buttonroku2").click(function(){
+    $("#screenroku").css("display", "none");
+    $("#screenoni").css("display", "block");
+});
+});
+$(document).ready(function(){
+$("#buttononi1").click(function(){
+    $("#screenoni").css("display", "none");
+    $("#start1").css("display", "block");
+});
+});
+$(document).ready(function(){
+$("#buttononi2").click(function(){
+    $("#screenoni").css("display", "none");
+    $("#screenkitsu").css("display", "block");
+});
+});
+$(document).ready(function(){
+$("#buttonkitsu1").click(function(){
+    $("#screenkitsu").css("display", "none");
+    $("#start1").css("display", "block");
+});
+});
+$(document).ready(function(){
+$("#buttonkitsu2").click(function(){
+    $("#screenkitsu").css("display", "none");
+    $("#screenshishi").css("display", "block");
+});
+});
+$(document).ready(function(){
+$("#buttonshishi1").click(function(){
+    $("#screenshishi").css("display", "none");
+    $("#start1").css("display", "block");
+});
+});
+$(document).ready(function(){
+$("#buttonshishi2").click(function(){
+    $("#screenshishi").css("display", "none");
+    $("#screenroku").css("display", "block");
+});
+});
